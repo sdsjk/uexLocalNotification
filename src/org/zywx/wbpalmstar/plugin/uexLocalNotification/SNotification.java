fa2e998c7b454f0c93b178ab510a0612afc12b99
张一博 <yibo.zhang@zymobi.com>
2018-12-28 19:00:37 +0800

    修复通知栏在8.0以上，没有设置channel导致通知栏不显示问题

--- src/org/zywx/wbpalmstar/plugin/uexLocalNotification/SNotification.java
+++ src/org/zywx/wbpalmstar/plugin/uexLocalNotification/SNotification.java
@@ -1,10 +1,13 @@
 package org.zywx.wbpalmstar.plugin.uexLocalNotification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.zywx.wbpalmstar.engine.EBrowserActivity;
@@ -13,6 +16,8 @@ import org.zywx.wbpalmstar.engine.universalex.EUExUtil;
 public class SNotification {
 
     public static int mId;
+    public static String customSound;
+
 
     @SuppressWarnings("deprecation")
     public static void notification(Context context, Alerm alerm) {
@@ -24,16 +29,79 @@ public class SNotification {
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
                 notyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         int iconId = EUExUtil.getResDrawableID("icon");
-        Notification notification = new Notification(iconId, alerm.title,
-                System.currentTimeMillis());
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
-        notification.defaults |= Notification.DEFAULT_SOUND; // 默认铃声
-        notification.setLatestEventInfo(context, alerm.title, alerm.content,
-                contentIntent);
+
+//        Notification notification = new Notification(iconId, alerm.title,
+//                System.currentTimeMillis());
+//        notification.flags = Notification.FLAG_AUTO_CANCEL;
+//        notification = initType(notification,alerm.getMode());
+//        notification.setLatestEventInfo(context, alerm.title, alerm.content,
+//                contentIntent);
+
         NotificationManager mMgr = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("my_channel_01", "customNotification",NotificationManager.IMPORTANCE_HIGH);
+            mMgr.createNotificationChannel(channel);
+            builder = new Notification.Builder(context, "my_channel_01");
+        }else {
+            builder = new Notification.Builder(context);
+        }
+//        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(iconId);
+        builder.setContentTitle(alerm.title);
+        builder.setContentText(alerm.content);
+        builder.setContentIntent(contentIntent);//执行intent
+        Notification notification = builder.getNotification();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notification=initType(notification,alerm.getMode()+"");
+
+
+
+        mId = alerm.notifyId.hashCode();
         mMgr.notify(mId, notification);
         EUexLocalNotify.addToMap(alerm.notifyId, mId);
         Log.e("==notification==", "===notification=end=");
+    }
+
+    /**
+     * set notify type
+     *
+     */
+    private static Notification initType(Notification notification, String ringType) {
+        if ("sound".equals(ringType)) {
+            // sound notification
+            if (TextUtils.isEmpty(customSound)) {
+                notification.defaults = Notification.DEFAULT_SOUND;
+            } else {
+                Uri sound = Uri.parse("file://" + customSound);
+                if (sound != null) {
+                    notification.sound = sound;
+                } else {
+                    notification.defaults = Notification.DEFAULT_SOUND;
+                }
+            }
+        } else if ("vibrate".equals(ringType)) {
+            // vibrate notification
+            notification.defaults = Notification.DEFAULT_VIBRATE;
+        } else if ("both".equals(ringType)) {
+            // vibrate and sound
+            if (TextUtils.isEmpty(customSound)) {
+                notification.defaults = Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND;
+            } else {
+                Uri sound = Uri.parse("file://" + customSound);
+                if (sound != null) {
+                    notification.sound = sound;
+                    notification.defaults = Notification.DEFAULT_VIBRATE;
+                } else {
+                    notification.defaults = Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND;
+                }
+            }
+        } else if("mute".equals(ringType)){
+            // nothing to do
+        } else if("system".equals(ringType) || "default".equals(ringType)){
+            notification.defaults |= Notification.DEFAULT_SOUND; // 默认铃声
+        }
+        return notification;
     }
 }
